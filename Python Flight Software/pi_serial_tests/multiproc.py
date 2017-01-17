03f5bf21729d9c3f41456f02b254569c364ab40f
Nick Mercadante <n.mercadante@yahoo.com>
2017-01-17 01:05:55 +0000

    Got the threading working on the test platform

--- Python Flight Software/pi_serial_tests/multiproc.py
+++ Python Flight Software/pi_serial_tests/multiproc.py
@@ -2,23 +2,24 @@ from multiprocessing import *
 from flat_file_getandset import *
 from time import sleep
 from picamera import *
+from datetime import *
 import sys
 import os
 
 
 def listen():
-  while True:
+  #while True:
     if getMessage() == 'pics':
       setTakePicture(1)
 
 def talk():
-  while True:
+  #while True:
     if getSendPicResponse() == '1':
       print 'Sending message!'
       setSendPicResponse(0)
 
 def printState():
-  while True:
+  #while True:
     print 'Message: ' + getMessage()
     print 'take_picture: ' + getTakePicture()
     print 'send_pic_response: ' + getSendPicResponse()
@@ -26,29 +27,48 @@ def printState():
     sleep(1)
 
 def cameraMonitor():
-  while True:
+  #while True:
     if getTakePicture() == '1':
       print 'Taking Picture!'
       setTakePicture(0)
       setMessage("")
       setSendPicResponse(1)
       #### FOR RASPBERRY PI ONLY ####
+      fn = '{:%m-%d-%Y_%H:%M:%S}'.format(datetime.now()) + '.jpg' # name with time
       cam = PiCamera()
-      cam.capture('tester.jpg')
+      cam.capture(fn)
       print 'Picture Taken'
       ###############################
 
 def setState(newstdin):
-  while True:
+  #while True:
     sys.stdin = newstdin
     answer = raw_input('Message[n to not change]: ')
     if answer.lower().strip() != 'n':
       setMessage(str(answer))
 
 if __name__ == '__main__':
-  job1 = Process(target=talk).start()
-  job2 = Process(target=listen).start()
-  job3 = Process(target=printState).start()
-  job4 = Process(target=cameraMonitor).start()
-  newstdin = os.fdopen(os.dup(sys.stdin.fileno()))
-  job5 = Process(target=setState, args=(newstdin,)).start()
+  while True:
+    job1 = Process(target=talk)
+    job2 = Process(target=listen)
+    job3 = Process(target=printState)
+    job4 = Process(target=cameraMonitor)
+    newstdin = os.fdopen(os.dup(sys.stdin.fileno()))
+    job5 = Process(target=setState, args=(newstdin,))
+
+    jobs = [job1, job2, job3, job4, job5]
+
+    for job in jobs:
+      job.start()
+      job.join()
+
+#  while len(jobs)>0:
+#    print 'Talk: ' + str(job1.is_alive())
+#    print 'Listen: ' + str(job1.is_alive())
+#    print 'printState: ' + str(job1.is_alive())
+#    print 'camMon: ' + str(job1.is_alive())
+#    sleep(0.5)
+#
+#    if not job4.is_alive():
+#      job4 = Process(target=cameraMonitor).start() # re-create process
+
